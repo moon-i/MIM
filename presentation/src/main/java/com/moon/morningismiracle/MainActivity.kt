ed83e0dba87492374fa03ad6f26c663fa464e20a
SEOMOON CHOI <seomoonchoi@SEOMOONui-MacBookAir.local>
2022-02-28 22:28:56 +0900

    [MIM-9] Material Design Bottom Navigation 적용하기
    - menu recource 생성
    - layout.xml 에 bottom navigation 사용하기
    - Navigation Item Select Listener 달기
    - 디자인 커스텀 하기 styles 적용
    
    정리된 notion - https://elfin-fin-b6d.notion.site/Bottom-navigation-Material-Design-c29d4742158d484d974b77c9e68052dd

--- presentation/src/main/java/com/moon/morningismiracle/MainActivity.kt
+++ presentation/src/main/java/com/moon/morningismiracle/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.moon.morningismiracle
 
 import android.os.Bundle
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationBarView
 import com.moon.morningismiracle.databinding.ActivityMainBinding
 import com.moon.morningismiracle.home.HomeFragment
 import com.moon.morningismiracle.plan.PlanFragment
@@ -11,17 +12,50 @@ import com.moon.morningismiracle.tag.TagFragment
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override var layoutResourceId: Int = R.layout.activity_main
 
-    val homeFragment by lazy { HomeFragment() }
-    val planFragment by lazy { PlanFragment() }
-    val tagFragment by lazy { TagFragment() }
-    val statisticsFragment by lazy { StatisticsFragment() }
+    private val homeFragment by lazy { HomeFragment() }
+    private val planFragment by lazy { PlanFragment() }
+    private val tagFragment by lazy { TagFragment() }
+    private val statisticsFragment by lazy { StatisticsFragment() }
+
+    private val itemSelectedListener =
+        NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.pageHome -> {
+                    switchFragment(homeFragment)
+                    true
+                }
+                R.id.pagePlan -> {
+                    switchFragment(planFragment)
+                    true
+                }
+                R.id.pageTag -> {
+                    switchFragment(tagFragment)
+                    true
+                }
+                R.id.pageStatistics -> {
+                    switchFragment(statisticsFragment)
+                    true
+                }
+                else -> false
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initView()
+    }
+
+    private fun initView() {
+        switchFragment(homeFragment)
+        binding.bottomNavigation.setOnItemSelectedListener(itemSelectedListener)
+        binding.bottomNavigation.itemRippleColor = null
+    }
+
+    private fun switchFragment(fragment: Fragment) {
         // FragmentManager를 통해서 FragmentTransaction 획득하기
         supportFragmentManager.beginTransaction().apply {
-            replace(binding.fragmentContainerView.id, homeFragment)
+            replace(binding.fragmentContainerView.id, fragment)
             setReorderingAllowed(true)
             commit()
         }
