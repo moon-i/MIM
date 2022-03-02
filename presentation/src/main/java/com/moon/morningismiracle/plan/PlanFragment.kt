3147f6cf93ae9f44f7aa3c9f374dc9cc6f473d76
moon.i <tjans1102@gmail.com>
2022-03-03 00:20:57 +0900

    [MIM-14] PlanFragment에 달력 넣기
    PlanFragment에 달력 UI 넣고 로직 넣기

--- presentation/src/main/java/com/moon/morningismiracle/plan/PlanFragment.kt
+++ presentation/src/main/java/com/moon/morningismiracle/plan/PlanFragment.kt
@@ -1,15 +1,77 @@
 package com.moon.morningismiracle.plan
 
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import com.moon.morningismiracle.BaseFragment
 import com.moon.morningismiracle.R
-import com.moon.morningismiracle.databinding.FragmentHomeBinding
+import com.moon.morningismiracle.custom_view.calendar.BackgroundDecorator
+import com.moon.morningismiracle.custom_view.calendar.PlanDecorator
+import com.moon.morningismiracle.custom_view.calendar.SaturdayDecorator
+import com.moon.morningismiracle.custom_view.calendar.SunDayDecorator
+import com.moon.morningismiracle.databinding.FragmentPlanBinding
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import java.util.*
 
-class PlanFragment : BaseFragment<FragmentHomeBinding>() {
+class PlanFragment : BaseFragment<FragmentPlanBinding>() {
     override var layoutResourceId: Int = R.layout.fragment_plan
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.calendarView.apply {
+            topbarVisible = false
+            val calendar = Calendar.getInstance()
+            val today: Date = calendar.time
+
+            calendar.add(Calendar.DAY_OF_YEAR, 1)
+            val tomorrow = calendar.time
+            setCurrentDate(today)
+
+            binding.dateTextView.text =
+                "${CalendarDay.today().year}년 ${CalendarDay.today().month + 1}월"
+
+            // TODO 실제데이터로 변경하기
+            val mydate = CalendarDay.from(2022, 2, 16)
+            val mydate2 = CalendarDay.from(2022, 2, 17)// year, month, date
+            val threeColors = intArrayOf(
+                Color.rgb(0, 0, 255),
+                Color.rgb(0, 255, 0),
+                Color.rgb(255, 0, 0),
+                Color.rgb(0, 0, 255),
+                Color.rgb(0, 255, 0),
+                Color.rgb(255, 0, 0),
+                Color.rgb(0, 0, 255),
+                Color.rgb(0, 255, 0),
+                Color.rgb(255, 0, 0)
+            )
+            addDecorator(PlanDecorator(hashSetOf(mydate, mydate2), threeColors))
+            addDecorator(SaturdayDecorator())
+            addDecorator(SunDayDecorator())
+            addDecorator(BackgroundDecorator(requireActivity()))
+            selectedDate = CalendarDay.today()
+
+            setOnMonthChangedListener { widget, date ->
+                date?.let { date ->
+                    binding.dateTextView.text = "${date.year}년 ${date.month + 1}월"
+                }
+            }
+
+            setOnDateChangedListener { widget, date, selected ->
+                // TODO open bottom sheet
+            }
+
+        }
+        binding.calendarLeftBtn.setOnClickListener {
+            binding.calendarView.goToPrevious()
+        }
+        binding.calendarRightBtn.setOnClickListener {
+            binding.calendarView.goToNext()
+        }
     }
 }
