88f591b366ba426df48c9d093c8755d1a46ed380
moon.i <tjans1102@gmail.com>
2022-03-04 23:39:36 +0900

    [MIM-17] StatisticsFragment view 구현
    통계화면을 구현합니다.
    필요한 drawable 구현,
    drawable 에 버전 상관없이 setTint할수있는 공통 util구현,
    mutate를 사용하여 한 drawable resource를 함께 사용하여 색상이 의도대로 동작하지않는 문제를 예방
    
    TODO - 이번달 구현, 실제 데이터로 연결

--- presentation/src/main/java/com/moon/morningismiracle/CommonUtil.kt
+++ presentation/src/main/java/com/moon/morningismiracle/CommonUtil.kt
@@ -2,6 +2,7 @@ package com.moon.morningismiracle
 
 import android.graphics.Color
 import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
 import androidx.core.graphics.drawable.DrawableCompat
 
 fun Drawable.setDrawableTint(color: String) {
@@ -10,5 +11,14 @@ fun Drawable.setDrawableTint(color: String) {
     DrawableCompat.setTint(
         drawable.mutate(),
         Color.parseColor(color)
+    )
+}
+
+fun Drawable.setDrawableTint(@ColorInt color: Int) {
+    val drawable: Drawable = DrawableCompat.wrap(this)
+
+    DrawableCompat.setTint(
+        drawable.mutate(),
+        color
     )
 }
--- presentation/src/main/java/com/moon/morningismiracle/statistics/StatisticsFragment.kt
+++ presentation/src/main/java/com/moon/morningismiracle/statistics/StatisticsFragment.kt
@@ -2,14 +2,60 @@ package com.moon.morningismiracle.statistics
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import com.moon.domain.model.TagStatisticsModel
 import com.moon.morningismiracle.BaseFragment
 import com.moon.morningismiracle.R
-import com.moon.morningismiracle.databinding.FragmentHomeBinding
+import com.moon.morningismiracle.databinding.FragmentStatisticsBinding
 
-class StatisticsFragment: BaseFragment<FragmentHomeBinding>() {
+class StatisticsFragment : BaseFragment<FragmentStatisticsBinding>() {
     override var layoutResourceId: Int = R.layout.fragment_statistics
+    private val tagStatisticsAdapter by lazy { TagStatisticsAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.tagStatisticsRecyclerView.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = tagStatisticsAdapter
+            isNestedScrollingEnabled = false
+        }
+
+        binding.totalWeekInfo.apply {
+            infoLayoutTitle.text = "총"
+            infoLayoutValue.text = "15건"
+            infoLayoutLogoImageView.setBackgroundResource(R.drawable.ic_all)
+        }
+
+        binding.failWeekInfo.apply {
+            infoLayoutTitle.text = "실패"
+            infoLayoutValue.text = "1건"
+            infoLayoutLogoImageView.setBackgroundResource(R.drawable.ic_fail)
+        }
+
+        binding.successWeekInfo.apply {
+            infoLayoutTitle.text = "성공"
+            infoLayoutValue.text = "4건"
+            infoLayoutLogoImageView.setBackgroundResource(R.drawable.ic_success)
+        }
+
+        // TODO TagState false인것 안보이게
+        // TODO 실제 데이터 연결하기
+        tagStatisticsAdapter.setData(
+            listOf(
+                TagStatisticsModel(3, "요가", "#DC5D6A", 2, 3),
+                TagStatisticsModel(4, "필테", "#E68765", 2, 3),
+                TagStatisticsModel(5, "일", "#E8C26B", 5, 7),
+                TagStatisticsModel(6, "먹기", "#A3CF77", 4, 7),
+                TagStatisticsModel(7, "사시", "#59C8AF", 100, 3054),
+                TagStatisticsModel(8, "정시", "#53ADCA", 23, 32),
+                TagStatisticsModel(9, "토익", "#5A8CD5", 12, 4),
+                TagStatisticsModel(10, "오픽", "#9E89D1", 1, 42)
+            )
+        )
     }
 }
