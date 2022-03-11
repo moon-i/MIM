61ef2a2d4c509ef216e36989c73e28aff3c58c77
moon.i <tjans1102@gmail.com>
2022-03-11 21:03:23 +0900

    [MIM-40] PlanTab에 계획 추가, 계획 삭제, 하단 plan list 구현
    기획을 변경 : 날짜 선택시 계획 추가 BS가 올라오던것을 -> Add 버튼을 따로 생성하고, 날짜 선택시 하단의 해당날짜 계획 list가 조회되도록 수정
    
    - Bottom sheet를 통해 계획추가 되도록 구현 -> 추가되면 하단의 리스트에 추가되도록,
    - 선택한 날짜에 해당하는 계획 list를 하단에 보여주도록 구현 : BS dismiss시 다시 List 조회되도록 구현
    - 계획 삭제 기능 연결

--- presentation/src/main/java/com/moon/morningismiracle/plan/PlanAdapter.kt
+++ presentation/src/main/java/com/moon/morningismiracle/plan/PlanAdapter.kt
@@ -3,6 +3,7 @@ package com.moon.morningismiracle.plan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -10,9 +11,11 @@ import com.moon.domain.model.PlanModel
 import com.moon.domain.model.PlanState
 import com.moon.morningismiracle.R
 import com.moon.morningismiracle.databinding.ItemPlanInCalendarTabBinding
+import com.moon.morningismiracle.di.DateInfo
 import com.moon.morningismiracle.setDrawableTint
 
 class PlanAdapter: ListAdapter<PlanModel, PlanAdapter.PlanViewHolder>(planDiffUtil) {
+    var onDeleteBtn: ((PlanModel) -> Unit)? = null
 
     inner class PlanViewHolder(private val binding: ItemPlanInCalendarTabBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: PlanModel) {
@@ -28,16 +31,38 @@ class PlanAdapter: ListAdapter<PlanModel, PlanAdapter.PlanViewHolder>(planDiffUt
                     planTagTextView.visibility = View.GONE
                     tagColorView.visibility = View.GONE
                 }
+
+                if (item.planDate < DateInfo.today) {
+                    deleteBtn.visibility = View.GONE
+                } else {
+                    deleteBtn.visibility = View.VISIBLE
+                    deleteBtn.setOnClickListener {
+                        onDeleteBtn?.invoke(item)
+                    }
+                }
             }
         }
 
         private fun setStateUi(state: PlanState) {
             binding.planStateTextView.text = state.uiValue // TODO Enum으로 빼서 상태 정보 mapping하기
             when (state) {
-                PlanState.SUCCESS -> binding.planStateBox.background.setDrawableTint(R.color.main_4, binding.root.context)
-                PlanState.FAIL -> binding.planStateBox.background.setDrawableTint(R.color.main_1, binding.root.context)
-                PlanState.WAITING -> binding.planStateBox.background.setDrawableTint(R.color.background_grey, binding.root.context)
-                PlanState.CANCEL -> binding.planStateBox.background.setDrawableTint(R.color.main_3, binding.root.context)
+                PlanState.SUCCESS -> {
+                    binding.planStateBox.background.setDrawableTint(R.color.plan_success, binding.root.context)
+                    binding.planStateTextView.setTextColor(getColor(binding.root.context, R.color.main_4))
+                }
+                PlanState.FAIL -> {
+                    binding.planStateBox.background.setDrawableTint(R.color.plan_cancel, binding.root.context)
+                    binding.planStateTextView.setTextColor(getColor(binding.root.context, R.color.main_1))
+                }
+                PlanState.WAITING -> {
+                    binding.planStateBox.background.setDrawableTint(R.color.background_light_grey, binding.root.context)
+                    binding.planStateTextView.setTextColor(getColor(binding.root.context, R.color.background_grey))
+
+                }
+                PlanState.CANCEL -> {
+                    binding.planStateBox.background.setDrawableTint(R.color.plan_fail, binding.root.context)
+                    binding.planStateTextView.setTextColor(getColor(binding.root.context, R.color.main_3))
+                }
             }
         }
     }
